cabb55caa8e95ab26887e94135fb1dc2ea9c98bd
Tiger Jove <tiger@jupiter.blue>
2024-02-06 19:04:36 +0100

    More integration tests, removed struct requirements fromt ypes

--- fennecs.tests/Integration/Scenarios.cs
+++ fennecs.tests/Integration/Scenarios.cs
@@ -3,15 +3,17 @@
 public class Scenarios
 {
     [Theory]
-    [InlineData(1_000)]
-    [InlineData(10_000)]
-    [InlineData(100_000)]
-    public void Can_Iterate_many_Entities(int count)
+    [InlineData(1_000, 2, 3, 5, 7)]
+    [InlineData(10_000, 2, 3, 5, 7)]
+    [InlineData(100_000, 19, 31, 37, 43)]
+    [InlineData(1_000_000, 23, 17, 19, 7)]
+    public void Can_Iterate_many_Entities(int count, int floatRate, int doubleRate, int stringRate, int shortRate)
     {
         using var world = new World();
         
         var random = new Random(9001);
         var entities = new List<Entity>();
+
 
         var floats = 0;
         var doubles = 0;
@@ -21,25 +23,25 @@ public class Scenarios
         for (var i = 1; i < count; i++)
         {
             var builder = world.Spawn().Add<int>(count);
-            if (i % 2 == 0)
+            if (i % floatRate == 0)
             {
                 floats++;
                 builder.Add<float>(i);
             }
 
-            if (i % 3 == 0)
+            if (i % doubleRate == 0)
             {
                 doubles++;
                 builder.Add<double>(i);
             }
 
-            if (i % 5 == 0)
+            if (i % stringRate == 0)
             {
                 strings++;
                 builder.Add(i.ToString());
             }
 
-            if (i % 7 == 0)
+            if (i % shortRate == 0)
             {
                 shorts++;
                 builder.Add<short>(entities[random.Next(0, entities.Count)]);
@@ -59,7 +61,10 @@ public class Scenarios
         Assert.Equal(strings, stringsActual);
 
         var stringsAndDoublesActual = world.Query<string, double>().Build().Count;
-        Assert.Equal(count/(5*3), stringsAndDoublesActual);
+        Assert.Equal(count / (stringRate * doubleRate), stringsAndDoublesActual);
+
+        var floatsAndShortsActual = world.Query().Any<float>().Has<short>(Identity.Any).Build().Count;
+        Assert.Equal(count / (floatRate * shortRate), floatsAndShortsActual);
 
         var shortsActual = world.Query().Has<short>(Identity.Any).Build().Count;
         Assert.Equal(shorts, shortsActual);
