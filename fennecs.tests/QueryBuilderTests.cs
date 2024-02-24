919ca1421d85351b98cee9e133b55a42792ebd45
Tiger Jove <tiger@jupiter.blue>
2024-02-24 19:48:20 +0100

    100% coverage restored

--- fennecs.tests/QueryBuilderTests.cs
+++ fennecs.tests/QueryBuilderTests.cs
@@ -3,6 +3,42 @@
 public class QueryBuilderTests
 {
     [Fact]
+    private void All_QueryBuilders_Available()
+    {
+        using var world = new World();
+        var q1 = world.Query();
+        var q2 = world.Query<int>();
+        var q3 = world.Query<int, string>();
+        var q4 = world.Query<int, string, double>();
+        var q5 = world.Query<int, string, double, float>();
+        var q6 = world.Query<int, string, double, float, long>();
+        Assert.NotNull(q1);
+        Assert.NotNull(q2);
+        Assert.NotNull(q3);
+        Assert.NotNull(q4);
+        Assert.NotNull(q5);
+        Assert.NotNull(q6);
+    }
+
+    [Fact]
+    private void All_QueryBuilders_Available_with_MatchExpressions()
+    {
+        using var world = new World();
+        var q1 = world.Query();
+        var q2 = world.Query<int>(Match.Identity);
+        var q3 = world.Query<int, string>(Match.Any, Match.Plain);
+        var q4 = world.Query<int, string, double>(Match.Object, Match.Relation, Match.Plain);
+        var q5 = world.Query<int, string, double, float>(Match.Object, Match.Relation, Match.Plain, Match.Any);
+        var q6 = world.Query<int, string, double, float, long>(Match.Object, Match.Relation, Match.Plain, Match.Any, Match.Object);
+        Assert.NotNull(q1);
+        Assert.NotNull(q2);
+        Assert.NotNull(q3);
+        Assert.NotNull(q4);
+        Assert.NotNull(q5);
+        Assert.NotNull(q6);
+    }
+
+    [Fact]
     private void Can_Create_Query()
     {
         using var world = new World();
