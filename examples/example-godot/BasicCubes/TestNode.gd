d02e997927577a76bdd8240274657bae198704a3
Tiger Jove <tiger@jupiter.blue>
2024-02-27 16:54:21 +0100

    Demo cleanup

--- examples/example-godot/BasicCubes/TestNode.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var ecsExample : MultiMeshExample
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
--- examples/example-godot/FPSLabel.gd
+++ examples/example-godot/FPSLabel.gd
@@ -1,15 +1,10 @@
 extends Label
 
 var smoothed : float = 0.016
-@onready var ECS : MultiMeshExample = %ECS
+@onready var ECS : MultiMeshExample = %MultiMeshExample
 @onready var VisibleSlider : VSlider = %VisibleSlider
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (delta > 0):
 		smoothed = smoothed * 0.95 + 0.05 * delta
