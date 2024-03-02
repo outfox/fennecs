91a7dc2990b0fcc20a039b44db70b0a030f46638
Tiger Jove <tiger@jupiter.blue>
2024-03-02 18:35:17 +0100

    Initial D example

---
+++ cookbook/InitialD.cs
@@ -0,0 +1,75 @@
+﻿// StarTrek.cs (type declarations at bottom of file)
+
+using fennecs;
+
+// 主催：藤原豆腐店
+var world = new World();
+
+// The Driver
+var takumi = new Driver("Takumi Fujiwara");
+
+// The Eight-Six
+var ae86 = world.Spawn()
+    .Add<Car>()
+    .Add<Name>("Toyota Sprinter Trueno AE86")
+    .Add(takumi)        // Add<Driver>
+    .Add(new Engine     // Add<Engine>
+    {
+        Horsepower = 130, Torque = 149,
+    }); 
+
+
+// All cars - driver check!
+var query = 
+    world.Query<Driver, Name>() // data we want to process
+    .Has<Car>()                 // additional Filter Expressions
+    .Build();
+
+query.For((ref Driver driver, ref Name name) =>
+{
+    driver.ReportForRace();
+    Console.WriteLine($"Car {name} is ready to go!");
+});
+
+// Adding component conditionally at runtime
+if (ae86.Has<Ready>()) ae86.Add<Racing>();
+
+// Removing component conditionally at runtime
+if (ae86.Has<Racing>()) ae86.Remove<Ready>();
+
+
+#region Components
+
+// "tags", size-less components we use to mark & classify entities
+internal struct Car;
+
+internal struct Ready;
+
+internal struct Racing;
+
+
+// data component
+internal struct Engine
+{
+    public float Horsepower;
+    public float Torque;
+}
+
+
+// data component wrapping another type, here: a string
+internal readonly struct Name(string value)
+{
+    public static implicit operator Name(string value) => new(value);
+    public override string ToString() => value;
+}
+
+
+// a class component, wrapping a string
+internal class Driver(string name)
+{
+    public void ReportForRace() => Console.WriteLine($"{name} is ready!");
+
+    public override string ToString() => name;
+}
+
+#endregion
--- fennecs.tech/cookbook/InitialD.md
+++ fennecs.tech/cookbook/InitialD.md
@@ -3,9 +3,31 @@ title: 2. Initial D (Components)
 outline: [2, 3]
 ---
 
-# Making a Sleeper
+# Tuning up a Sleeper
 
-::: warning :neofox_what: WHAT THE...
-Sorry, work in progress! come back soon.
+::: info :neofox_floof_mug: MMMH, REAL CODE
+This **RUNS**! *Playful premises aside*, this is a functioning showcase of **fenn**ecs principles.
+
+Get comfy, grab a cup of ~~Java~~ ~~CoffeeScript~~ ~~Visual J#~~ whatever, and get your paws dirty playing around in the code! It's good fun!
+
+All `.csproj` and `.cs` files are [over here on Github!](https://github.com/thygrrr/fennecs/blob/main/cookbook) 
+
 :::
 
+### Premise
+Let's build a sleeper drift racing championship car out of nothing but Entities and Components!
+
+An easy intro to **fenn**ecs where you won't spill your tofu yet!
+
+### Implementation
+<<< ../../cookbook/StarTrek.cs {cs:line-numbers}
+
+### Outcome
+The above code is the actual code, yet this output is copy-pasta and may not be re-generated each time the code is updated. *(mumble CI-CD mumble TODO mumble)*
+```shell
+dotnet run --project StarTrek.csproj
+```
+```txt
+Takumi Fujiwara is ready!
+Car Toyota Sprinter Trueno AE86 is ready to go!
+```
