dd682f31fdb87642608445b3e8b292663466ee24
Tiger Jove <tiger@jupiter.blue>
2024-03-06 09:47:39 +0100

    Reordering World.CRUD

--- fennecs/World.CRUD.cs
+++ fennecs/World.CRUD.cs
@@ -5,35 +5,6 @@ namespace fennecs;
 public partial class World
 {
     #region CRUD
-    private void RemoveComponent(Identity identity, TypeExpression typeExpression)
-    {
-        if (Mode == WorldMode.Deferred)
-        {
-            _deferredOperations.Enqueue(new DeferredOperation {Opcode = Opcode.Remove, Identity = identity, TypeExpression = typeExpression});
-            return;
-        }
-
-        ref var meta = ref _meta[identity.Index];
-
-        var oldArchetype = meta.Archetype;
-
-        if (!oldArchetype.Signature.Contains(typeExpression))
-        {
-            throw new ArgumentException($"Entity {identity} does not have a component of type {typeExpression}");
-        }
-
-        var newSignature = oldArchetype.Signature.Remove(typeExpression);
-        var newArchetype = GetArchetype(newSignature);
-        var newRow = Archetype.MoveEntry(identity, meta.Row, oldArchetype, newArchetype);
-
-        meta.Row = newRow;
-        meta.Archetype = newArchetype;
-    }
-    #endregion
-
-
-
-    #region Component Interaction
     /// <summary>
     /// Creates an Archetype relation between this identity and an object (instance of a class).
     /// The relation is backed by the object itself, which will be enumerated by queries if desired.
@@ -180,18 +151,38 @@ public partial class World
         ref var meta = ref _meta[identity.Index];
         var oldArchetype = meta.Archetype;
 
-        if (oldArchetype.Signature.Contains(typeExpression))
-        {
-            throw new ArgumentException($"Entity {identity} already has a component of type {typeExpression}");
-        }
-        
+        if (oldArchetype.Signature.Contains(typeExpression)) throw new ArgumentException($"Entity {identity} already has a component of type {typeExpression}");
+
         var newSignature = oldArchetype.Signature.Add(typeExpression);
         var newArchetype = GetArchetype(newSignature);
         var newRow = Archetype.MoveEntry(identity, meta.Row, oldArchetype, newArchetype);
-        
+
         // Back-fill the new value
         newArchetype.Set(typeExpression, data, newRow);
-        
+
+        meta.Row = newRow;
+        meta.Archetype = newArchetype;
+    }
+
+
+    private void RemoveComponent(Identity identity, TypeExpression typeExpression)
+    {
+        if (Mode == WorldMode.Deferred)
+        {
+            _deferredOperations.Enqueue(new DeferredOperation {Opcode = Opcode.Remove, Identity = identity, TypeExpression = typeExpression});
+            return;
+        }
+
+        ref var meta = ref _meta[identity.Index];
+
+        var oldArchetype = meta.Archetype;
+
+        if (!oldArchetype.Signature.Contains(typeExpression)) throw new ArgumentException($"Entity {identity} does not have a component of type {typeExpression}");
+
+        var newSignature = oldArchetype.Signature.Remove(typeExpression);
+        var newArchetype = GetArchetype(newSignature);
+        var newRow = Archetype.MoveEntry(identity, meta.Row, oldArchetype, newArchetype);
+
         meta.Row = newRow;
         meta.Archetype = newArchetype;
     }
