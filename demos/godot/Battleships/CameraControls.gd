59551a54e1397fe514c4629bddae797fbabb02b3
Tiger Jove <tiger@jupiter.blue>
2024-03-10 00:59:20 +0100

    Fixed mouse focus zoom

--- demos/godot/Battleships/CameraControls.gd
+++ demos/godot/Battleships/CameraControls.gd
@@ -5,7 +5,11 @@ extends Camera2D
 
 @onready var parent : Node2D = $".."
 
+@export var maxZoom : float = 5.0
+@export var minZoom : float = 0.1
+
 var last_mouse : Vector2
+var zoom_mouse : Vector2
 
 func zoom_and_pan() -> void:
 	var current_mouse := get_local_mouse_position()
@@ -14,13 +18,15 @@ func zoom_and_pan() -> void:
 
 	if Input.is_action_just_pressed("look>zoom+"):
 		zoom_goal *= 1.1111111
-	zoom_goal = Vector2(clampf(zoom_goal.x, 0.1, 5), clampf(zoom_goal.y, 0.1, 5))
-
+		zoom_mouse = get_viewport().get_mouse_position()
+		zoom_mouse -= get_viewport_rect().size * 0.5
 
 	if Input.is_action_just_pressed("look>zoom-"):
 		zoom_goal *= 0.9
-		zoom_goal = Vector2(clampf(zoom_goal.x, 0.1, 5), clampf(zoom_goal.y, 0.1, 5))
+		zoom_mouse = get_viewport().get_mouse_position()
+		zoom_mouse -= get_viewport_rect().size * 0.5
 
+	zoom_goal = zoom_goal.clamp(minZoom * Vector2.ONE, maxZoom * Vector2.ONE)
 	last_mouse = current_mouse
 
 
@@ -28,14 +34,11 @@ func zoom_and_pan() -> void:
 func _process(delta: float) -> void:
 	zoom_and_pan()
 
-	var k := pow(0.9, 120.0*delta)
+	var k := pow(0.85, 120.0*delta)
 
-	var view_mouse = get_viewport().get_mouse_position()
-	view_mouse -= get_viewport_rect().size * 0.5
-
-	var mouse_pre_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(view_mouse))
+	var mouse_pre_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(zoom_mouse))
 	zoom = zoom * k + (1.0-k) * zoom_goal
-	var mouse_post_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(view_mouse))
+	var mouse_post_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(zoom_mouse))
 
 	var zoom_position_offset := (mouse_pre_zoom - mouse_post_zoom)
 
