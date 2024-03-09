3cfec903e67f8d4c186fd853e727da5d5a731fdd
Tiger Jove <tiger@jupiter.blue>
2024-03-10 00:50:02 +0100

    Fixed mouse focus zoom

--- demos/godot/Battleships/CameraControls.gd
+++ demos/godot/Battleships/CameraControls.gd
@@ -14,7 +14,7 @@ func zoom_and_pan() -> void:
 
 	if Input.is_action_just_pressed("look>zoom+"):
 		zoom_goal *= 1.1111111
-		zoom_goal = Vector2(clampf(zoom_goal.x, 0.1, 5), clampf(zoom_goal.y, 0.1, 5))
+	zoom_goal = Vector2(clampf(zoom_goal.x, 0.1, 5), clampf(zoom_goal.y, 0.1, 5))
 
 
 	if Input.is_action_just_pressed("look>zoom-"):
@@ -29,13 +29,15 @@ func _process(delta: float) -> void:
 	zoom_and_pan()
 
 	var k := pow(0.9, 120.0*delta)
-	var mouse_pre_zoom := get_local_mouse_position()
+
+	var view_mouse = get_viewport().get_mouse_position()
+	view_mouse -= get_viewport_rect().size * 0.5
+
+	var mouse_pre_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(view_mouse))
 	zoom = zoom * k + (1.0-k) * zoom_goal
-	var mouse_post_zoom := get_local_mouse_position()
+	var mouse_post_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(view_mouse))
 
 	var zoom_position_offset := (mouse_pre_zoom - mouse_post_zoom)
-	print(zoom, zoom_position_offset)
 
 	position_goal += zoom_position_offset
-	position += zoom_position_offset
-	position = position * k + (1.0-k) * position_goal
+	position = position * k + (1.0-k) * position_goal + zoom_position_offset
