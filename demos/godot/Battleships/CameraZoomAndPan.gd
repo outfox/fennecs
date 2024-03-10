c206611db7b23286e6785b59bb3ad65464eb4a7f
Tiger Jove <tiger@jupiter.blue>
2024-03-10 12:47:21 +0100

    Smoothdamp for the camera.

--- demos/godot/Battleships/CameraZoomAndPan.gd
+++ demos/godot/Battleships/CameraZoomAndPan.gd
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Unlicense or CC0
 extends Node2D
 
-# Smooth panning and precise zooming forCamera2D
+# Smooth panning and precise zooming for Camera2D
 # Usage: This script may be placed on a child node
 # of a Camera2D or on a Camera2D itself.
 # Suggestion: Change and/or set up the three Input Actions,
@@ -11,7 +11,6 @@ extends Node2D
 class_name CameraZoomAndPan
 
 @onready var camera : Camera2D = $".." if ($".." is Camera2D) else self
-
 
 #region Exported Parameters
 @export_range(1, 20, 0.01) var maxZoom : float = 5.0
@@ -25,30 +24,15 @@ class_name CameraZoomAndPan
 
 
 @export_group("Mouse")
-@export_enum("Auto", "Always", "Never") var mouseFallback: String = "Auto"
+@export var zoomToCursor: bool = true
+@export_enum("Auto", "Always", "Never") var useFallbackButtons: String = "Auto"
 @export var panButton : MouseButton = MOUSE_BUTTON_MIDDLE
 @export var zoomInButton : MouseButton = MOUSE_BUTTON_WHEEL_UP
 @export var zoomOutButton : MouseButton = MOUSE_BUTTON_WHEEL_DOWN
 
 @export_group("Smoothing")
-
-@export_range(0, 0.99, 0.01) var panSmoothing : float = 0.5:
-	set(new_value):
-		panSmoothing = pow(new_value, slider_exponent)
-	get:
-		return panSmoothing
-
-@export_range(0, 0.99, 0.01) var zoomSmoothing : float = 0.5:
-	set(new_value):
-		zoomSmoothing = pow(new_value, slider_exponent)
-	get:
-		return zoomSmoothing
-
-# To make the sliders be pleasantly non-linear
-const slider_exponent : float = 0.25
-
-# To make the smoothing ratios framerate-independent
-const referenceFPS : float = 120.0
+@export_range(0, 0.5, 0.01) var panSmoothing : float = 0.1
+@export_range(0, 0.5, 0.01) var zoomSmoothing : float = 0.1
 #endregion
 
 
@@ -62,17 +46,16 @@ var fallback_mouse_zoom_out : bool
 var last_mouse : Vector2
 var zoom_mouse : Vector2
 
-# We need to do manually re-assign the editor-serialized values
-# because the initial editor value doesn't go through the setter
-func _ready() -> void:
-	panSmoothing = panSmoothing
-	zoomSmoothing = zoomSmoothing
+@onready var damped_pan: Array[Vector2] = [camera.position, Vector2.ZERO]
+@onready var damped_zoom: Array[Vector2] = [camera.zoom, Vector2.ZERO]
 
+
+func _ready() -> void:
 	# If the actions aren't defined and mouse fallback is enabled,
 	# use the default mouse buttons
 	var actions = InputMap.get_actions()
-	var always = mouseFallback == "Always"
-	var never = mouseFallback == "Never"
+	var always = useFallbackButtons == "Always"
+	var never = useFallbackButtons == "Never"
 	fallback_mouse_pan = not never and (always or (panAction not in actions))
 	fallback_mouse_zoom_in = not never and (always or (zoomInAction not in actions))
 	fallback_mouse_zoom_out = not never and (always or (zoomOutAction not in actions))
@@ -82,27 +65,38 @@ func _ready() -> void:
 			panAction + "=" + str(fallback_mouse_pan),
 			zoomInAction + "=" + str(fallback_mouse_zoom_in),
 			zoomOutAction + "=" + str(fallback_mouse_zoom_out))
-		printt("CameraZoomAndPan: TIP - set up all three of the following InputActions:", panAction, zoomInAction, zoomOutAction)
+		printt("CameraZoomAndPan: TIP - set up all three of the following InputActions:",
+			panAction,
+			zoomInAction,
+			zoomOutAction)
 #endregion
 
 
 func _process(delta: float) -> void:
-	# Calculate FIR / invExp kernels for smoothing
-	var k_pan := pow(panSmoothing, referenceFPS * delta)
-	var k_zoom := pow(zoomSmoothing, referenceFPS * delta)
+	_SmoothDamp(damped_zoom, zoom_goal, zoomSmoothing, delta)
 
+	# Zoom in and determine camera offset to keep
+	# the view under the mouse cursor
 	var mouse_pre_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(zoom_mouse))
-	camera.zoom = camera.zoom * k_zoom + (1.0-k_zoom) * zoom_goal
+	camera.zoom = damped_zoom[0]
 	var mouse_post_zoom := to_local(get_canvas_transform().affine_inverse().basis_xform(zoom_mouse))
 
-	var zoom_position_offset := (mouse_pre_zoom - mouse_post_zoom)
+	var zoom_position_offset := (mouse_pre_zoom - mouse_post_zoom) if zoomToCursor else Vector2.ZERO
 
 	position_goal += zoom_position_offset
-	camera.position = camera.position * k_pan + (1.0-k_pan) * position_goal + zoom_position_offset
+	damped_pan[0] += zoom_position_offset
+
+
+	_SmoothDamp(damped_pan, position_goal, panSmoothing, delta)
+	camera.position = damped_pan[0]
+
 
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventMouse and not event is InputEventAction:
+		return
+
 	var current_mouse := get_local_mouse_position()
 	if Input.is_action_pressed(panAction) or (fallback_mouse_pan and Input.is_mouse_button_pressed(panButton)):
 		position_goal += (last_mouse - current_mouse)
@@ -119,3 +113,44 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	zoom_goal = zoom_goal.clamp(minZoom * Vector2.ONE, maxZoom * Vector2.ONE)
 	last_mouse = current_mouse
+
+
+
+
+func _SmoothDamp(state: Array[Vector2], target : Vector2, smoothTime : float, deltaTime : float):
+		var current := state[0]
+		var linear_velocity := state[1]
+
+		if smoothTime == 0:
+			state[0] = target
+			state[1] = Vector2.ZERO
+			return
+
+		var omega := 2.0 / smoothTime
+
+		var x := omega * deltaTime;
+		var exp := 1.0 / (1.0 + x + 0.48 * x * x + 0.235 * x * x * x);
+
+		var change := current - target;
+		var originalTo := target;
+
+		# Optional: Clamp maxSpeed
+		# var maxChange = maxSpeed * smoothTime;
+		# change = clamp(change, -maxChange, maxChange);
+		target = current - change;
+
+		var temp := (linear_velocity + omega * change) * deltaTime
+		linear_velocity = (linear_velocity - omega * temp) * exp
+		var output := target + (change + temp) * exp
+
+		# Prevent overshooting - FIXME
+		# likely needs to treat all components separately
+		if (originalTo.x > current.x) == (output.x > originalTo.x):
+			output.x = originalTo.x
+			linear_velocity.x = (output.x - originalTo.x) / deltaTime
+		if (originalTo.y > current.y) == (output.y > originalTo.y):
+			output.y = originalTo.y
+			linear_velocity.y = (output.y - originalTo.y) / deltaTime
+
+		state[0] = output
+		state[1] = linear_velocity
--- demos/godot/Battleships/Ship.cs
+++ demos/godot/Battleships/Ship.cs
@@ -7,5 +7,9 @@ public partial class Ship : Sprite2D
 {
 	[Export] public Sprite2D[] Guns;
 
-
+	public override void _Ready()
+	{
+		base._Ready();
+		//TODO: Hook up to ECS world.
+	}
 }
