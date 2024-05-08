5e6a003a412bb433904637e514f5867fef922061
Tiger Jove <tiger@jupiter.blue>
2024-05-08 14:59:22 +0200

    Fresh docs and example for QueryBuilder.

--- fennecs/QueryBuilder.cs
+++ fennecs/QueryBuilder.cs
@@ -6,39 +6,91 @@ using fennecs.pools;
 
 namespace fennecs;
 
-public class QueryBuilder : IDisposable
+/// <summary>
+/// Fluent builder interface which compiles Queries via its Build() method.
+/// </summary>
+/// <para>
+/// A QueryBuilder serves to specify inclusion/exclusion criteria for entities and
+/// their components, and then compiles them into fast Queries via its Build() method.
+/// </para>
+/// <para>
+/// Example use:
+/// <code>
+/// <![CDATA[
+/// var world = new fennecs.World();
+/// 
+/// var selectedHealthBars = world.Query<HP, HPBar>()
+///     .Has<Selected>()
+///     .Any<Player>()
+///     .Any<NPC>()
+///     .Not<Disabled>()
+///     .Build();
+///
+/// 
+/// selectedHealthBars.For(
+///     (ref HP hp, ref HPBar bar) =>
+///     {
+///         bar.Fill = hp.Cur/hp.Max;
+///     });
+/// ]]>
+/// </code>
+/// </para>
+/// <remarks>
+/// Compilation is reasonably fast, and cached.
+/// A Query with the same Mask of criteria will be pulled from the cache if it was already compiled. 
+/// You can compile multiple queries from the same builder (adding more criteria as you go).
+/// </remarks>
+public abstract class QueryBuilder : IDisposable
 {
+    #region Internals
+
     internal readonly World World;
     internal readonly Mask Mask = MaskPool.Rent();
 
     private protected readonly PooledList<TypeExpression> StreamTypes = PooledList<TypeExpression>.Rent();
-
-    /* TODO: Implement deferred builder
-    private List<ValueTuple<Type, Identity, object>> _has;
-    private List<ValueTuple<Type, Identity, object>> _not;
-    private List<ValueTuple<Type, Identity, object>> _any;
-    */
-
 
     internal QueryBuilder(World world)
     {
         World = world;
     }
 
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="target"></param>
-    /// <typeparam name="T"></typeparam>
-    protected void Outputs<T>(Identity target = default)
+    private protected void Outputs<T>(Identity target = default)
     {
         var typeExpression = TypeExpression.Of<T>(target);
         StreamTypes.Add(typeExpression);
         Mask.Has(typeExpression);
     }
 
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Mask.Dispose();
+        StreamTypes.Dispose();
+    }
 
+    #endregion
+
+    
+    #region Public API
+
+    /// <summary>
+    /// Builds (compiles) the Query from the current state of the QueryBuilder.
+    /// </summary>
+    /// <remarks>
+    /// This method is covariant, so you will get the appropriate stream Query subclass
+    /// depending on the Stream Types (type parameters) you passed to <see cref="fennecs.World.Query{C}()"/>
+    /// or any of its overloads.
+    /// </remarks>
+    /// <returns>compiled query (you can compile more than one query from the same builder)</returns>
+    public abstract Query Build();
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public virtual QueryBuilder Has<T>(Identity target = default)
     {
         var typeExpression = TypeExpression.Of<T>(target);
@@ -49,6 +101,12 @@ public class QueryBuilder : IDisposable
     }
 
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public virtual QueryBuilder Has<T>(T target) where T : class
     {
         Mask.Has(TypeExpression.Of<T>(Identity.Of(target)));
@@ -56,6 +114,12 @@ public class QueryBuilder : IDisposable
     }
 
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public virtual QueryBuilder Not<T>(Identity target = default)
     {
         Mask.Not(TypeExpression.Of<T>(target));
@@ -63,6 +127,12 @@ public class QueryBuilder : IDisposable
     }
 
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public virtual QueryBuilder Not<T>(T target) where T : class
     {
         var typeExpression = TypeExpression.Of<T>(Identity.Of(target));
@@ -72,6 +142,12 @@ public class QueryBuilder : IDisposable
     }
 
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public virtual QueryBuilder Any<T>(Identity target = default)
     {
         Mask.Any(TypeExpression.Of<T>(target));
@@ -79,21 +155,22 @@ public class QueryBuilder : IDisposable
     }
 
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public virtual QueryBuilder Any<T>(T target) where T : class
     {
         Mask.Any(TypeExpression.Of<T>(Identity.Of(target)));
         return this;
     }
 
-
-    public void Dispose()
-    {
-        Mask.Dispose();
-        StreamTypes.Dispose();
-    }
+    #endregion
 }
 
-
+/// <inheritdoc />
 public sealed class QueryBuilder<C1> : QueryBuilder
 {
     private static readonly Func<World, List<TypeExpression>, Mask, List<Archetype>, Query> CreateQuery =
@@ -106,49 +183,56 @@ public sealed class QueryBuilder<C1> : QueryBuilder
     }
 
 
-    public Query<C1> Build()
+    /// <inheritdoc />
+    public override Query<C1> Build()
     {
         return (Query<C1>) World.GetQuery(StreamTypes, Mask, CreateQuery);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1> Has<T>(Identity target = default)
     {
         return (QueryBuilder<C1>) base.Has<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1> Has<T>(T target) where T : class
     {
         return (QueryBuilder<C1>) base.Has(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1> Not<T>(Identity target = default)
     {
         return (QueryBuilder<C1>) base.Not<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1> Not<T>(T target) where T : class
     {
         return (QueryBuilder<C1>) base.Not(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1> Any<T>(Identity target = default)
     {
         return (QueryBuilder<C1>) base.Any<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1> Any<T>(T target) where T : class
     {
         return (QueryBuilder<C1>) base.Any(target);
     }
 }
 
-
+/// <inheritdoc />
 public sealed class QueryBuilder<C1, C2> : QueryBuilder
 {
     private static readonly Func<World, List<TypeExpression>, Mask, List<Archetype>, Query> CreateQuery =
@@ -162,49 +246,56 @@ public sealed class QueryBuilder<C1, C2> : QueryBuilder
     }
 
 
-    public Query<C1, C2> Build()
+    /// <inheritdoc />
+    public override Query<C1, C2> Build()
     {
         return (Query<C1, C2>) World.GetQuery(StreamTypes, Mask, CreateQuery);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2> Has<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2>) base.Has<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2> Has<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2>) base.Has(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2> Not<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2>) base.Not<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2> Not<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2>) base.Not(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2> Any<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2>) base.Any<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2> Any<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2>) base.Any(target);
     }
 }
 
-
+/// <inheritdoc />
 public sealed class QueryBuilder<C1, C2, C3> : QueryBuilder
 {
     private static readonly Func<World, List<TypeExpression>, Mask, List<Archetype>, Query> CreateQuery =
@@ -219,49 +310,57 @@ public sealed class QueryBuilder<C1, C2, C3> : QueryBuilder
     }
 
 
-    public Query<C1, C2, C3> Build()
+    /// <inheritdoc />
+    /// <inheritdoc />
+    public override Query<C1, C2, C3> Build()
     {
         return (Query<C1, C2, C3>) World.GetQuery(StreamTypes, Mask, CreateQuery);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3> Has<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3>) base.Has<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3> Has<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3>) base.Has(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3> Not<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3>) base.Not<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3> Not<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3>) base.Not(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3> Any<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3>) base.Any<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3> Any<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3>) base.Any(target);
     }
 }
 
-
+/// <inheritdoc />
 public sealed class QueryBuilder<C1, C2, C3, C4> : QueryBuilder
 {
     private static readonly Func<World, List<TypeExpression>, Mask, List<Archetype>, Query> CreateQuery =
@@ -277,49 +376,56 @@ public sealed class QueryBuilder<C1, C2, C3, C4> : QueryBuilder
     }
 
 
-    public Query<C1, C2, C3, C4> Build()
+    /// <inheritdoc />
+    public override Query<C1, C2, C3, C4> Build()
     {
         return (Query<C1, C2, C3, C4>) World.GetQuery(StreamTypes, Mask, CreateQuery);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4> Has<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3, C4>) base.Has<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4> Has<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3, C4>) base.Has(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4> Not<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3, C4>) base.Not<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4> Not<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3, C4>) base.Not(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4> Any<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3, C4>) base.Any<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4> Any<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3, C4>) base.Any(target);
     }
 }
 
-
+/// <inheritdoc />
 public sealed class QueryBuilder<C1, C2, C3, C4, C5> : QueryBuilder
 {
     private static readonly Func<World, List<TypeExpression>, Mask, List<Archetype>, Query> CreateQuery =
@@ -336,42 +442,49 @@ public sealed class QueryBuilder<C1, C2, C3, C4, C5> : QueryBuilder
     }
 
 
-    public Query<C1, C2, C3, C4, C5> Build()
+    /// <inheritdoc />
+    public override Query<C1, C2, C3, C4, C5> Build()
     {
         return (Query<C1, C2, C3, C4, C5>) World.GetQuery(StreamTypes, Mask, CreateQuery);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4, C5> Has<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3, C4, C5>) base.Has<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4, C5> Has<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3, C4, C5>) base.Has(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4, C5> Not<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3, C4, C5>) base.Not<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4, C5> Not<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3, C4, C5>) base.Not(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4, C5> Any<T>(Identity target = default)
     {
         return (QueryBuilder<C1, C2, C3, C4, C5>) base.Any<T>(target);
     }
 
 
+    /// <inheritdoc />
     public override QueryBuilder<C1, C2, C3, C4, C5> Any<T>(T target) where T : class
     {
         return (QueryBuilder<C1, C2, C3, C4, C5>) base.Any(target);
--- fennecs/World.Builders.cs
+++ fennecs/World.Builders.cs
@@ -2,36 +2,94 @@
 
 public partial class World
 {
+    /// <summary>
+    /// Creates a fluent Builder for a query with only the Identity component as its sole Stream Type.
+    /// </summary>
+    /// <remarks>
+    /// A query with zero stream types seemed nonsensical. 💌 Feedback is welcome, what's your use case?
+    /// </remarks>
+    /// <returns><see cref="QueryBuilder{Identity}"/></returns>
     public QueryBuilder<Identity> Query()
     {
         return new QueryBuilder<Identity>(this);
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with one output Stream Type.
+    /// </summary>
+    /// <remarks>
+    /// Compile the Query from the builder using its <see cref="QueryBuilder.Build"/> method.
+    /// </remarks>
+    /// <typeparam name="C">component type that runners of this query will have access to</typeparam>
+    /// <returns><see cref="QueryBuilder{C1}"/></returns>
     public QueryBuilder<C> Query<C>()
     {
         return new QueryBuilder<C>(this, Match.Any);
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with one output Stream Type.
+    /// A <see cref="Match"/> expression can be specified to limit the components matched to Stream Types, for instance:
+    /// <see cref="Match.Any"/>, <see cref="Match.Entity"/>, <see cref="Match.Object"/>, <see cref="Match.Plain"/> or <see cref="Match.Target"/>.
+    /// </summary>
+    /// <remarks>
+    /// This bakes the Match Expression into the compiled Query, which is slightly more performant than using Query.<see cref="fennecs.Query.AddFilter{T}"/> and much more performant than using Query.<see cref="fennecs.Query.Filtered"/>.
+    /// </remarks>
+    /// <param name="match">Match Expression</param>
+    /// <typeparam name="C">component type that runners of this query will have access to</typeparam>
+    /// <returns><see cref="QueryBuilder{C1}"/></returns>
     public QueryBuilder<C> Query<C>(Identity match)
     {
         return new QueryBuilder<C>(this, match);
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with two output Stream Types.
+    /// </summary>
+    /// <remarks>
+    /// Compile the Query from the builder using its <see cref="QueryBuilder.Build"/> method.
+    /// </remarks>
+    /// <typeparam name="C1">component Stream Type 1</typeparam>
+    /// <typeparam name="C2">component Stream Type 2</typeparam>
+    /// <returns><see cref="QueryBuilder{C1, C2}"/></returns>
     public QueryBuilder<C1, C2> Query<C1, C2>()
     {
         return new QueryBuilder<C1, C2>(this, Match.Any, Match.Any);
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with two output Stream Types.
+    /// A <see cref="Match"/> expression can be specified to limit the components matched to Stream Types, for instance:
+    /// <see cref="Match.Any"/>, <see cref="Match.Entity"/>, <see cref="Match.Object"/>, <see cref="Match.Plain"/> or <see cref="Match.Target"/>.
+    /// </summary>
+    /// <remarks>
+    /// This bakes the Match Expression into the compiled Query, which is slightly more performant than using Query.<see cref="fennecs.Query.AddFilter{T}"/> and much more performant than using Query.<see cref="fennecs.Query.Filtered"/>.
+    /// </remarks>
+    /// <param name="match1">Match Expression for Stream Type 1</param>
+    /// <param name="match2">Match Expression for Stream Type 2</param>
+    /// <typeparam name="C1">component Stream Type 1</typeparam>
+    /// <typeparam name="C2">component Stream Type 2</typeparam>
+    /// <returns><see cref="QueryBuilder{C1, C2}"/></returns>
     public QueryBuilder<C1, C2> Query<C1, C2>(Identity match1, Identity match2)
     {
         return new QueryBuilder<C1, C2>(this, match1, match2);
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with three output Stream Types.
+    /// </summary>
+    /// <remarks>
+    /// Compile the Query from the builder using its <see cref="QueryBuilder.Build"/> method.
+    /// </remarks>
+    /// <typeparam name="C1">component Stream Type 1</typeparam>
+    /// <typeparam name="C2">component Stream Type 2</typeparam>
+    /// <typeparam name="C3">component Stream Type 2</typeparam>
+    /// <returns><see cref="QueryBuilder{C1, C2, C3}"/></returns>
     public QueryBuilder<C1, C2, C3> Query<C1, C2, C3>()
     {
         return new QueryBuilder<C1, C2, C3>(this, Match.Any, Match.Any, Match.Any);
@@ -44,6 +102,17 @@ public partial class World
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with four output Stream Types.
+    /// </summary>
+    /// <remarks>
+    /// Compile the Query from the builder using its <see cref="QueryBuilder.Build"/> method.
+    /// </remarks>
+    /// <typeparam name="C1">component Stream Type 1</typeparam>
+    /// <typeparam name="C2">component Stream Type 2</typeparam>
+    /// <typeparam name="C3">component Stream Type 3</typeparam>
+    /// <typeparam name="C4">component Stream Type 4</typeparam>
+    /// <returns><see cref="QueryBuilder{C1, C2, C3, C4}"/></returns>
     public QueryBuilder<C1, C2, C3, C4> Query<C1, C2, C3, C4>()
     {
         return new QueryBuilder<C1, C2, C3, C4>(this, Match.Any, Match.Any, Match.Any, Match.Any);
@@ -56,6 +125,18 @@ public partial class World
     }
 
 
+    /// <summary>
+    /// Creates a fluent Builder for a query with five output Stream Types.
+    /// </summary>
+    /// <remarks>
+    /// Compile the Query from the builder using its <see cref="QueryBuilder.Build"/> method.
+    /// </remarks>
+    /// <typeparam name="C1">component Stream Type 1</typeparam>
+    /// <typeparam name="C2">component Stream Type 2</typeparam>
+    /// <typeparam name="C3">component Stream Type 3</typeparam>
+    /// <typeparam name="C4">component Stream Type 4</typeparam>
+    /// <typeparam name="C5">component Stream Type 5</typeparam>
+    /// <returns><see cref="QueryBuilder{C1, C2, C3, C4, C5}"/></returns>
     public QueryBuilder<C1, C2, C3, C4, C5> Query<C1, C2, C3, C4, C5>()
     {
         return new QueryBuilder<C1, C2, C3, C4, C5>(this, Match.Any, Match.Any, Match.Any, Match.Any, Match.Any);
