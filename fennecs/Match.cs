0d4968f70c112c0b5c81188166c0c82996e03af0
Tiger Jove <tiger@jupiter.blue>
2024-06-01 11:43:29 +0200

    Nailing down the new Match Syntax.

--- fennecs/Match.cs
+++ fennecs/Match.cs
@@ -1,12 +1,20 @@
-﻿namespace fennecs;
+﻿using System.Runtime.InteropServices;
 
-public readonly record struct Match(Identity Value)
+namespace fennecs;
+
+public readonly record struct Match 
 {
+    internal Identity Value { get; init; }
+    
+    private Match(Identity Value) => this.Value = Value;
+
+    public static Match Entity(Entity other) => new(other.Id);
+    public static Match Object<T>(T link) where T : class => new(Identity.Of<T>(link));
+
     public static Match Any => new(MatchOld.Any);
     public static Match AnyRelation => new(MatchOld.Target);
     public static Match AnyObject => new(MatchOld.Object);
     public static Match AnyEntity => new(MatchOld.Entity);
-    public static Match Entity(Entity other) => new(other.Id);
     public static Match Plain => new(MatchOld.Plain);
     
     public bool Matches(Match other) => Value == other.Value;
@@ -22,4 +30,9 @@ public readonly record struct Match(Identity Value)
     
     [Obsolete("Find a way to remove me.")]
     internal ulong Raw => Value.Value;
+    
+    public void Deconstruct(out Identity Value)
+    {
+        Value = this.Value;
+    }
 }
