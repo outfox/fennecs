05e76dd332f928b2dcbda5243be3c4b6c99a97dc
Tiger Jove <tiger@jupiter.blue>
2024-06-03 10:35:26 +0200

    Starting with Unit tests

--- fennecs/expressions/MatchExpression.cs
+++ fennecs/expressions/MatchExpression.cs
@@ -1,24 +1,20 @@
 ﻿namespace fennecs.expressions;
 
-public readonly record struct Match
+internal readonly record struct Match(TypeExpression value)
 {
-    private Match(TypeExpression value)
-    {
-        this.value = value;
-    }
-
     public static Match Any<T>() => new(TypeExpression.Of<T>(MatchOld.Any));
     public static Match AnyEntity<T>() => new(TypeExpression.Of<T>(MatchOld.Entity));
     public static Match AnyTarget<T>() => new(TypeExpression.Of<T>(MatchOld.Target));
     public static Match AnyObject<T>() => new(TypeExpression.Of<T>(MatchOld.Object));
     public static Match OnlyPlain<T>() => new(TypeExpression.Of<T>(MatchOld.Plain));
 
+    public static Match Plain<T>() => new(TypeExpression.Of<T>(MatchOld.Plain));
     public static Match Entity<T>(Entity target) => new(TypeExpression.Of<T>(target));
     public static Match Object<T>(T target) where T : class => new(TypeExpression.Of<T>(Link.With(target)));
     
     internal bool Matches(TypeExpression other) => value.Matches(other);
-
-    private TypeExpression value { get; init; }
+    internal bool Matches(Match other) => value.Matches(other.value);
+    internal bool Matches(Component other) => value.Matches(other.value);
 }
 
 
@@ -29,5 +25,6 @@ internal readonly record struct Component(TypeExpression value)
     public static Component Object<T>(T target) where T : class => new(TypeExpression.Of<T>(Link.With(target)));
     
     internal bool Matches(TypeExpression other) => value.Matches(other);
+    internal bool Matches(Match other) => value.Matches(other.value);
+    internal bool Matches(Component other) => value.Matches(other.value);
 }
-
