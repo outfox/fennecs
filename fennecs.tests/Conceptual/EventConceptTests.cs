e12a60a40b6ec394aa51305e9a556700af82dd1c
Tiger Jove <tiger@jupiter.blue>
2024-11-03 14:09:46 +0100

    Sketch for Component change events.

---
+++ fennecs.tests/Conceptual/EventConceptTests.cs
@@ -0,0 +1,96 @@
+﻿namespace fennecs.tests.Conceptual;
+
+public class EventConceptTests
+{
+    public EventConceptTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    private static ITestOutputHelper _output = null!;
+
+    public interface IAdded<in T>
+    {
+        static void Added(Entity entity, T value)
+        {
+            _output.WriteLine($"Added {value}");
+        }
+    }
+
+    public interface IValue<T>
+    {
+        T Value { get; set; }
+    }
+
+    public interface IModified<in C>
+    {
+        static void Modified(C value)
+        {
+            _output.WriteLine($"Modified {value}");
+        }
+    }
+
+    public interface IRemoved<in T>
+    {
+        static void Removed(Entity entity, T value)
+        {
+            _output.WriteLine($"Removed {value}");
+        }
+    }
+
+    public record struct TestComponent(int Value) :
+        IValue<int>,
+        IAdded<TestComponent>,
+        IModified<TestComponent>,
+        IRemoved<TestComponent>
+    {
+        private int _value = Value;
+
+        public int Value
+        {
+            readonly get => _value;
+            set
+            {
+                _value = value;
+                IModified<TestComponent>.Modified(this);
+            }
+        }
+    }
+
+    [Fact]
+    public void Test()
+    {
+        using var world = new World();
+        var entity = world.Spawn();
+
+        var component = new TestComponent(1);
+        entity.TestAdd(component);
+
+        component.Value = 2;
+        component.Value = 3;
+        component.Value = 4;
+
+        entity.TestRemove<TestComponent>();
+    }
+}
+
+public static class EntityExtensions
+{
+    public static void TestAdd<T>(this Entity entity, T component) where T : notnull
+    {
+        entity.Add(component);
+        if (component is EventConceptTests.IAdded<T>)
+        {
+            EventConceptTests.IAdded<T>.Added(entity, component);
+        }
+    }
+
+    public static void TestRemove<T>(this Entity entity) where T : EventConceptTests.IRemoved<T>
+    {
+        entity.Remove<T>();
+        if (typeof(T).IsAssignableTo(typeof(EventConceptTests.IRemoved<T>)))
+        {
+            EventConceptTests.IRemoved<T>.Removed(entity, default!);
+        }
+    }
+}
